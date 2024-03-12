a1e36f3c34900e6e2145c79ecd652528b00fba1e
JayYu0116 <l2377020625@outlook.com>
2024-03-12 15:28:19 -0400

    change port for lab

--- test_inference.sh
+++ test_inference.sh
@@ -3,7 +3,7 @@
 # https://stackoverflow.com/questions/75096564/unrecognized-content-type-parameters-format-when-serving-model-on-databricks-ex
 # https://mlflow.org/docs/latest/deployment/deploy-model-locally.html#local-inference-server-spec
 
-curl http://127.0.0.1:5001/invocations -H 'Content-Type: application/json' -d '{
+curl http://127.0.0.1:6001/invocations -H 'Content-Type: application/json' -d '{
   "dataframe_split": {
     "columns": ["input1", "input2", "input3", "input4"],
     "data": [[1, 2, 3, 4]]
