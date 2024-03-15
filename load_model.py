180b692d5f97a63c9d778b2c3b1e215eb9bc27ce
Yiwen Zhao <122934755+yiwenzcmu@users.noreply.github.com>
2024-03-14 22:33:46 -0400

    Modified train_model.py and load_model.py

--- load_model.py
+++ load_model.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 # TODO: Set tht MLFlow server uri
-uri = ___________________
+uri = 'http://127.0.0.1:6001'
 mlflow.set_tracking_uri(uri=uri)
 
 # TODO: Provide model path/url
-logged_model = ___________________
+logged_model = 'runs:/a63f9ed02e1b48e995276a1bf1dd46cd/iris_model'
 
 # Load model as a PyFuncModel.
 loaded_model = mlflow.sklearn.load_model(logged_model)
@@ -17,7 +17,7 @@ data=np.array([[1.0,2.0,3.0,4.0]])
 
 # TODO: Predict on a Pandas DataFrame. Due to the MLFlow functionality constrain.
 #       The loaded model's predict function only accept dataframe as input instead of numpy array.
-prediction=loaded_model.predict(___________)
+prediction=loaded_model.predict(pd.DataFrame(data))
 
 # Print out prediction result
 print(prediction)
--- train_model.py
+++ train_model.py
@@ -7,33 +7,32 @@ from utility import pipeline
 from mlflow.models import infer_signature
 
 # TODO: Set tht MLFlow tracking server uri
-uri = _________
+uri = 'http://127.0.0.1:6001'
 # Use mlflow.set_tracking_uri to set the uri
 mlflow.set_tracking_uri(uri)
 
 # Set experiment name
-email="______@andrew.cmu.edu" # TODO: Use a customized experiment name
+email="yiwenz5@andrew.cmu.edu" # TODO: Use a customized experiment name
 experiment_name=f"{email}-lab7"
 mlflow.set_experiment(experiment_name)
 
 # Begin the model training and evaluation process
 
 # TODO: Generates train and test dataset using `pipeline.data_preprocessing` function
-X_train,X_test,y_train,y_test= ________
+X_train,X_test,y_train,y_test= pipeline.data_preprocessing()
 
 # Train the model:
 params = {
     "solver": "lbfgs",
-    "max_iter": 1000,:
+    "max_iter": 1000,
     "multi_class": "auto",
     "random_state": 8888,
 }
 
 # TODO: Use `pipeline.train_logistic_regression` to generate trained model
-trained_model=__________________
-
+trained_model= pipeline.train_logistic_regression(X_train, y_train, params)
 # TODO: use `pipeline.evaluation` to evaluate the model
-accuracy=_____________________
+accuracy = pipeline.evaluation(trained_model, X_test, y_test)
 
 # Log model and metrics to tracking serverhost>
 # Start an MLflow run
